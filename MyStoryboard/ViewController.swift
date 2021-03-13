1ccf6261168f73fb35ade5f7b18af831d89825e3
naoki-kkc <naoki2kuchi@gmail.com>
2021-03-14 03:02:35 +0900

    Initial Commit

---
+++ MyStoryboard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyStoryboard
+//
+//  Created by ginger893 on 2021/03/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
